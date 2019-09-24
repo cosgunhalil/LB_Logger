2c541201dd45c0872dac99766608cd109eb9dfec
Halil Coşgun <cosgun.halil@gmail.com>
2019-09-25 00:58:28 +0300

    feat: log item view add

---
+++ Lopul Bodon Logger/Assets/LB_Logger/BaseScripts/LogMonitor/LogItemView.cs
@@ -0,0 +1,35 @@
+﻿namespace Helpers.Logger
+{
+    using UnityEngine;
+    using TMPro;
+    using UnityEngine.UI;
+
+    public class LogItemView : MonoBehaviour
+    {
+        [SerializeField]
+        private TextMeshProUGUI idContainer;
+        [SerializeField]
+        private TextMeshProUGUI logContainer;
+        [SerializeField]
+        private Image backgroundImage;
+
+        public void Setup(ref LogItemData data)
+        {
+            idContainer.text = data.Id;
+            logContainer.text = data.log;
+        }
+
+        private void SetColor(Color color)
+        {
+            backgroundImage.color = color;
+        }
+    }
+
+    public struct LogItemData
+    {
+        public readonly string Id;
+        public readonly string log;
+    }
+
+}
+
